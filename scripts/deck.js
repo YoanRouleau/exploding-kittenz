1af56d8b739bafca81eec5eec0895fc6ab57b3d0
cannonbark <geoffrey.parrier@gmail.com>
2023-09-07 20:59:52 +0200

    Feat: Separating and making some classes

---
+++ scripts/deck.js
@@ -0,0 +1,46 @@
+import cards from '../public/assets/cards.json';
+
+/**
+ * @typedef Card
+ * @property {string} id        The card id
+ * @property {string} name      The card name
+ * @property {string} effect    What the card do
+ */
+
+export class Deck {
+
+    /**
+     * Create Deck
+     *
+     * @param {boolean} isGameDeck Check if a deck is for the game and not for players
+     */
+    constructor({ isGameDeck } = { isGameDeck: false }) {
+        /**
+         *
+         * @type {Card[]}
+         * @private
+         */
+        this._cards = [];
+        if (isGameDeck) {
+            this._cards = cards;
+        }
+    }
+    get cards() {
+        return this._cards;
+    }
+
+    get size() {
+        return this._cards.length;
+    }
+
+    add(cardId) {
+        if (typeof cardId !== "string") {
+            throw new TypeError("Function add of Deck class needs type string for cardId.");
+        }
+        if (!Object.keys(cards).includes(cardId)) {
+            throw new ReferenceError("Function add of Deck class needs a valid Card Id.");
+        }
+        this._cards.push(cardId);
+    }
+}
+
